4ed96a24c1efbadc581391a442c9f59e5cda5023
Achraf Belarif <abelarif@e2r5p4.1337.ma>
2021-06-10 14:12:26 +0100

    push today

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,36 +1,18 @@
-        global  _ft_strcmp
+section .text
+global	_ft_strcmp
 
 _ft_strcmp:
-        xor rax, rax
-
-        mov rax, -1
-        jmp ft_loop
-
-ft_loop:
-        inc rax
-        mov cl, rdi[rax]
-        mov dl, rsi[rax]
-        cmp cl, 0
-        je  ft_diff
-        cmp dl, 0
-        je  ft_diff
-        cmp cl, dl
-        jne ft_diff
-        jmp ft_loop
-
-ft_diff:
-        sub cl, dl
-        xor rax, rax
-
-        cmp cl, 0
-        jl ft_neg
-
-        mov al, cl
-        ret
-
-ft_neg:
-        sub al, cl
-        xor rbx, rbx
-        mov rbx, -1
-        imul rax, rbx
-        ret
+	xor	rax, rax
+	xor	rbx, rbx
+	LOOP:
+		mov	r12b, byte [rdi + rbx]
+		mov	r13b, byte [rsi + rbx]
+		cmp	r12b, r13b
+		jne END
+		inc	rbx
+		cmp r12b, 0
+		jne	LOOP
+	END:
+		mov rax, r12
+		sub rax, r13
+		ret
