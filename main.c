e1d8ce01a010bc9b78b5b545d27e50a688c1ce38
Achraf Belarif <abelarif@e2r5p4.1337.ma>
2021-06-10 15:40:42 +0100

    fix errors

--- main.c
+++ main.c
@@ -3,84 +3,97 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abelarif <abelarif@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: abelarif <abelarif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 16:49:12 by abelarif          #+#    #+#             */
-/*   Updated: 2021/04/22 10:52:27 by abelarif         ###   ########.fr       */
+/*   Updated: 2021/06/10 15:40:11 by abelarif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libasm.h"
 
-// void        test_ft_read(int fildes, void *buf, size_t nbyte)
-// {
-    
-// }
-
-// void        test_ft_strcmp(void)
-// {
-//     const char *s2 = "\xff\xff";
-//     const char *s1 = "\xff";
-//     printf("SYS : %d\n",    strcmp(s1, s2));
-//     printf("USR : %d\n", ft_strcmp(s1, s2));    
-// }
-
-
-// void        test_ft_strdup(const char *s1)
-// {}
-
-// void        test_ft_write(int fildes, const void *buf, size_t nbyte)
-// {
-//     int     sys, usr;
-
-//     sys = 0;
-//     usr = 0;
-    
-//     usr = ft_write(fildes, buf, nbyte);
-//     printf("\nUSR : %d\n", usr);
-//     printf("USR_ERRNO : %d\n", errno);
-    
-//     printf("*****************************************************************************************\n");
-    
-//     sys = write(fildes, buf, nbyte);
-//     printf("\nSYS : %d\n", sys);
-//     printf("SYS_ERRNO : %d\n", errno);
-// }
-
-// void        test_ft_strcpy(void)
-// {
-//     char    dst0[] = "acbe0123456789";
-//     char    dst1[] = "acbe0123456789";
-//     char    src0[] = "1325     ";
-//     char    src1[] = "1325     ";
-
-//     printf("SYS : >>%s<<\n",    strcpy(dst0, src0));
-//     printf("USR : >>%s<<\n", ft_strcpy(dst1, src1));
-// }
-
-
-// void        test_ft_strlen(const char *s)
-// {
-//     printf("SYS : %lu\n", strlen(s));
-//     printf("USR : %zu\n", ft_strlen(s));
-// }
-
-void    test_ft_strdup(char *str0)
+void    read_test(int fd, size_t nbyte)
 {
-    char *s1;
+    int         sys;
+    int         usr;
+    char        *sysbuf = malloc(sizeof(char) * 500);
+    char        *usrbuf = malloc(sizeof(char) * 500);
 
-    s1 = ft_strdup(str0);
-    printf(">>%s<<\n", s1);
+    sys = read(fd, sysbuf, nbyte);
+    usr = ft_read(fd, usrbuf, nbyte);
+    printf("USR : [%d], [%s]\n", usr, usrbuf);
+    printf("SYS : [%d], [%s]\n", sys, sysbuf);
 }
 
-int     main()
+void       strcmp_test(char *str0, char *str1)
 {
-    char    *str0 = "str";
-    // test_ft_read(0, str0, "5");
-    // test_ft_strcmp();
-    // test_ft_strcpy();
-    test_ft_strdup(str0);
-    // test_ft_strlen("verrbw\0teenrewthqe\\");
-    // test_ft_write(15, str0, 16);
+    int         sys;
+    int         usr;
+
+    sys = strcmp(str0, str1);
+    usr = ft_strcmp(str0, str1);
+    printf("SYS : [%d]\n", sys);
+    printf("USR : [%d]\n", usr);
+}
+
+void    strcpy_test()
+{
+    char    *dstusr = "DESTINATION";
+    char    *srcusr = "SOURCE";
+    char    *dstsys = "DESTINATION";
+    char    *srcsys = "SOURCE";
+
+    dstsys = strcpy(dstsys, srcsys);
+    dstusr = strcpy(dstusr, srcusr);
+    printf("SYS : [%s]\n", dstsys);
+    printf("USR : [%s]\n", dstusr);
+}
+
+void    strdup_test(char *str)
+{
+    char    *sys;
+    char    *usr;
+
+    sys = strdup(str);
+    usr = ft_strdup(str);
+    printf("SYS : [%s]\n", sys);
+    printf("USR : [%s]\n", usr);
+}
+
+void    strlen_test(char *str)
+{
+    size_t  sys;
+    size_t  usr;
+
+    sys = strlen(str);
+    usr = ft_strlen(str);
+    printf("SYS : [%zu]\n", sys);
+    printf("USR : [%zu]\n", usr);
+}
+
+void    write_test(int fd, void *buf, size_t nbyte)
+{
+    int     sys;
+    int     usr;
+
+    printf("SYS : \n");
+        sys = write(fd, buf, nbyte);
+    printf("USR : \n");
+        usr = ft_write(fd, buf, nbyte);
+    printf("SYS : [%d]\n", sys);
+    printf("USR : [%d]\n", usr);
+}
+
+int     main(void)
+{
+    // char    *str0 = "str0";
+    // char    *str1 = "str1";
+
+    read_test(0, 5);
+    // strcmp_test(str0, str1);
+    // strcpy_test();
+    // strdup_test(str0);
+    // strlen_test(str0);
+    // write_test(15, str0, 16);
     return (0);
 }
